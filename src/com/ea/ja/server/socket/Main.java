2ada21379885d60f0d2c7aeaed5c4862550d30cf
achesnoiu <achesnoiu@ea.com>
2016-04-28 17:44:36 +0300

    Server class added.

--- src/com/ea/ja/server/socket/Main.java
+++ src/com/ea/ja/server/socket/Main.java
@@ -6,7 +6,7 @@ package com.ea.ja.server.socket;
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("mata");
+        System.out.println("test");
 
 
     }
